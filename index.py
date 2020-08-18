a9e038c42158262e4268bfc4b998726fddf34a8d
eeejeenee <2014102505@khu.ac.kr>
2020-08-18 18:11:33 +0900

    first commit

---
+++ index.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+response = urlopen('https://www.daum.net/')
+soup = BeautifulSoup(response, 'html.parser')
+i = 1
+f = open("D:/eeejeenee/coding/웹+앱-조코딩/animal/새파일.txt", 'w')
+for anchor in soup.select("a.link_favorsch"):
+    data = str(i) + "위: " + anchor.get_text() + "\n"
+    i += 1
+    f.write(data)
+f.close()
